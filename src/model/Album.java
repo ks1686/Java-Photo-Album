0e86cf4be2fcf08d1bd5895ed0e29670eb7bf19c
jacobjude <jacob.jude03@gmail.com>
2024-04-03 12:20:42 -0400

    implemented photo search in album class
    untested. also added another photo to the stock iamges

--- src/model/Album.java
+++ src/model/Album.java
@@ -2,6 +2,7 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 
 public class Album {
@@ -89,10 +90,89 @@ public class Album {
         return "Album: " + this.albumName + "\nPhotos:\n" + result;
     }
 
-    /*
-    public List<Photo> search(query) {
-        return null; // TODO: Implement search (for date and tags)
+    
+    public List<Photo> search(String query) {
+        // query can be a tag or a Calendar date
+        // check if the query is a calendar date
+        List<Photo> result = new ArrayList<>();
+        if (query.matches("\\d{4}-\\d{2}-\\d{2}")) {
+            // query is a date
+            for (Photo photo : this.photos) {
+                if (photo.getDate().toString().equals(query)) {
+                    result.add(photo);
+                }
+            }
+            return result;
+        }
+
+        // tags can look like "tagname=tagvalue" and can have conjunctions or disjuctions
+        // ex. person=John AND location=New York
+        // ex. person=John OR location=New York
+        // no need to handle more than 1 conjunction or disjunction
+
+        // split the query by " AND " or " OR "
+        String[] parts = query.split(" AND | OR ");
+
+        // if the query is a single tag
+        if (parts.length == 1) {
+            String[] tag = query.split("=");
+            for (Photo photo : this.photos) {
+                for (Map<String, String> currentTag : photo.getTags()) {
+                    if (currentTag.containsKey(tag[0]) && currentTag.containsValue(tag[1])) {
+                        result.add(photo);
+                    }
+                }
+            }
+            return result;
+        } else if (parts.length == 2) {
+            // if the query is a disjunction
+            if (!query.contains(" AND ")) {
+                String[] tag1 = parts[0].split("=");
+                String[] tag2 = parts[1].split("=");
+                for (Photo photo : this.photos) {
+                    boolean found1 = false;
+                    boolean found2 = false;
+                    for (Map<String, String> tag : photo.getTags()) {
+                        if (tag.containsKey(tag1[0]) && tag.containsValue(tag1[1])) {
+                            found1 = true;
+                        }
+                        if (tag.containsKey(tag2[0]) && tag.containsValue(tag2[1])) {
+                            found2 = true;
+                        }
+                    }
+                    if (found1 && found2) {
+                        result.add(photo);
+                    }
+                }
+                return result;
+            }
+            
+        } else {
+            // if the query is a conjunction
+            String[] tag1 = parts[0].split("=");
+            String[] tag2 = parts[1].split("=");
+            for (Photo photo : this.photos) {
+                boolean found1 = false;
+                boolean found2 = false;
+                for (Map<String, String> tag : photo.getTags()) {
+                    if (tag.containsKey(tag1[0]) && tag.containsValue(tag1[1])) {
+                        found1 = true;
+                    }
+                    if (tag.containsKey(tag2[0]) && tag.containsValue(tag2[1])) {
+                        found2 = true;
+                    }
+                }
+                if (found1 || found2) {
+                    result.add(photo);
+                }
+            }
+            return result;
+        }
+
+        
+        return result;  
     }
     
-     */
+    
+     
 }
