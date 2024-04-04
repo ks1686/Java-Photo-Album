37936ccf999d661334c82df8256c08a32176afe1
Karim Smires <96136962+ks1686@users.noreply.github.com>
2024-04-03 21:01:57 -0400

    Number of photos in album
    AlbumListController now shows the number of photos in each album.

--- src/view/AlbumListController.java
+++ src/view/AlbumListController.java
@@ -21,23 +21,36 @@ public class AlbumListController {
         // go through data/{username}/ and get all the album names
         // add them to the list
         
-        File userDir = new File("data/" + username);
-        File[] files = userDir.listFiles();
-        for (File f : files) {
-            if (f.isDirectory()) {
-                albums.add(f.getName());
+        File userDir = new File("data/" + username); // get the user's directory
+        File[] files = userDir.listFiles(); // get all the files in the user's directory
+        for (File f : files) { // go through all the files
+            if (f.isDirectory()) { // if it's a directory
+                albums.add(f.getName()); // add the name to the list
             }
         }
 
+        // add the number of photos in each album to the list
+        for (int i = 0; i < albums.size(); i++) {
+            File albumDir = new File("data/" + username + "/" + albums.get(i)); // get the album's directory
+            File[] albumFiles = albumDir.listFiles(); // get all the files in the album's directory
+            int numPhotos = 0; // initialize the number of photos
+            for (File f : albumFiles) { // go through all the files
+                if (f.isFile()) { // if it's a file
+                    numPhotos++; // increment the number of photos
+                }
+            }
+            albums.set(i, albums.get(i) + " (" + numPhotos + " photos)"); // add the number of photos to the album name
+        }
 
-        System.out.println(albums);
 
-        obsList = FXCollections.observableArrayList(albums);
-        albumListView.setItems(obsList);
+        System.out.println(albums); // print out the list of albums
 
-        albumListView.getSelectionModel().select(0);
+        obsList = FXCollections.observableArrayList(albums); // create an observable list from the list of albums
+        albumListView.setItems(obsList); // set the list view to the observable list
 
-        albumListView.getSelectionModel().selectedIndexProperty().addListener((obsList, oldVal, newVal) -> showItem(stage));
+        albumListView.getSelectionModel().select(0); // select the first item in the list
+
+        albumListView.getSelectionModel().selectedIndexProperty().addListener((obsList, oldVal, newVal) -> showItem(stage)); // add a listener to the list view
 
         // print out albumListView's items
         System.out.println("Albums: " + albumListView.getItems());
@@ -46,6 +59,6 @@ public class AlbumListController {
     }
 
     private void showItem(Stage mainstage) {
-        System.out.println("Selected item: " + albumListView.getSelectionModel().getSelectedItem());
+        System.out.println("Selected item: " + albumListView.getSelectionModel().getSelectedItem()); // print out the selected item
     }
 }
